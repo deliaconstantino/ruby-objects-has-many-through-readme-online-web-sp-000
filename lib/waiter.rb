9f4ee26923cecb8f3af4ea272c1ac8ebf1be7ccb
Delia Constantino <dconstan92@gmail.com>
2020-11-12 16:10:14 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,9 +25,9 @@ class Waiter
     end
   end
 
-  def best_tipper
-    self.meals.sort { |a, b| b.tip <=> a.tip }.first.customer
-  end
+  # def best_tipper
+  #   self.meals.sort { |a, b| b.tip <=> a.tip }.first.customer
+  # end
 
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
